984f6341c125a37eb613300bb7365311a401784a
Dan <Mackachooo@gmail.com>
2024-03-06 15:56:04 +0000

    Dan...

--- src/FruitSalad.java
+++ src/FruitSalad.java
@@ -2,7 +2,9 @@ public class FruitSalad {
 
     public static void main(String[] args) {
         String[] fruits = {"Kiwi Fruit", "Gala Apple", "Granny Smith Apple", "Cherry Tomato", "Gooseberry",
-                "Beefsteak Tomato", "Braeburn Apple", "Blueberry", "Strawberry", "Navel Orange", "Pink Pearl Apple",  "Raspberry", "Blood Orange", "Sungold Tomato", "Fuji Apple", "Blackberry", "Banana", "Pineapple", "Florida Orange", "Kiku Apple", "Mango", "Satsuma Orange", "Watermelon", "Snozzberry"};
+                "Beefsteak Tomato", "Braeburn Apple", "Blueberry", "Strawberry", "Navel Orange", "Pink Pearl Apple",
+                "Raspberry", "Blood Orange", "Sungold Tomato", "Fuji Apple", "Blackberry", "Banana", "Pineapple",
+                "Florida Orange", "Kiku Apple", "Mango", "Satsuma Orange", "Watermelon", "Snozzberry"};
 
         String[] fruitSalad = new String[12];
         int fruitCount = 0;
