40a6349602ffab5d1dc275727232a868267dbff1
Dan <Mackachooo@gmail.com>
2024-03-06 17:17:29 +0000

    Dan...

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner McScan = new Scanner(System.in);
+        int n;
+        do {
+        System.out.println("What number would you like to factor? ");
+        n = McScan.nextInt();
+        } while(n <= 0);
 
-       System.out.printf("Hello and welcome!");
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                factors.add(i);
+            }
+        }
 
+        System.out.println("The factors of "+ n +" are:");
+        for (Integer factor: factors) {
+            System.out.print(factor + " ");
+        }
+        System.out.println(n +" has "+ (factors.size() + 1) +" factors.");
+        System.out.println(n +" is "+ ((sumList(factors) == n) ? "" : "not ") +"a perfect number.");
+        System.out.println(n +" is "+ ((factors.size() == 1) ? "" : "not ") +"a prime number.");
+
+    }
+
+    static int sumList(List<Integer> nums) {
+        int result = 0;
+        for (int num: nums) {
+            result += num;
+        }
+        return result;
     }
 }
