503b6221e8170b310f5917346bbcf5e0ecd2d45c
Dan <Mackachooo@gmail.com>
2024-03-06 17:25:21 +0000

    Fix

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner McScan = new Scanner(System.in);
+        Scanner mcScan = new Scanner(System.in);
         int n;
+
+        // Checks for correct n
         do {
         System.out.println("What number would you like to factor? ");
-        n = McScan.nextInt();
+        n = mcScan.nextInt();
         } while(n <= 0);
 
+        // Finds Factors
         List<Integer> factors = new ArrayList<>();
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 factors.add(i);
             }
         }
 
+        // Prints Results
         System.out.println("The factors of "+ n +" are:");
         for (Integer factor: factors) {
             System.out.print(factor + " ");
         }
-        System.out.println(n +" has "+ (factors.size() + 1) +" factors.");
+        System.out.println("\n"+n +" has "+ (factors.size()) +" factors.");
         System.out.println(n +" is "+ ((sumList(factors) == n) ? "" : "not ") +"a perfect number.");
-        System.out.println(n +" is "+ ((factors.size() == 1) ? "" : "not ") +"a prime number.");
+        System.out.println(n +" is "+ ((factors.size() == 2) ? "" : "not ") +"a prime number.");
 
     }
 
