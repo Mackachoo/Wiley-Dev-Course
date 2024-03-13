767188acecdad6984a0f7f7416f9e0ed9d6c4187
Dan <Mackachooo@gmail.com>
2024-03-13 17:39:31 +0000

    Step 2: Application Shell

---
+++ src/com/sg/classroster/controller/ClassRosterController.java
@@ -0,0 +1,6 @@
+package com.sg.classroster.controller;
+
+public class ClassRosterController {
+    ClassRosterView classView = new ClassRosterView();
+    ClassRosterDao classDao = new ClassRosterDaoFileImpl();
+}
