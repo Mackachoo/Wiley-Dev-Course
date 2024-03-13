36114fe0d5693551ff6e28a68d6b3980ca8e5191
Dan <Mackachooo@gmail.com>
2024-03-13 13:20:52 +0000

    Dan...

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,10 @@
+import java.io.*;
 import java.util.*;
 
-import static java.util.Map.entry;
 public class Main {
 
-    final static Map<String, String> statecapitals = Map.ofEntries(
-            entry("Alabama",	"Montgomery"),
-            entry("Alaska",	"Juneau"),
-            entry("Arizona",	"Phoenix"),
-            entry("Arkansas",	"Little Rock"),
-            entry("California",	"Sacramento"),
-            entry("Colorado",	"Denver"),
-            entry("Connecticut",	"Hartford"),
-            entry("Delaware",	"Dover"),
-            entry("Florida",	"Tallahassee"),
-            entry("Georgia",	"Atlanta"),
-            entry("Hawaii",	"Honolulu"),
-            entry("Idaho",	"Boise"),
-            entry("Illinois",	"Springfield"),
-            entry("Indiana",	"Indianapolis"),
-            entry("Iowa",	"Des Moines"),
-            entry("Kansas",	"Topeka"),
-            entry("Kentucky",	"Frankfort"),
-            entry("Louisiana",	"Baton Rouge"),
-            entry("Maine",	"Augusta"),
-            entry("Maryland",	"Annapolis"),
-            entry("Massachusetts",	"Boston"),
-            entry("Michigan",	"Lansing"),
-            entry("Minnesota",	"Saint Paul"),
-            entry("Mississippi",	"Jackson"),
-            entry("Missouri",	"Jefferson City"),
-            entry("Montana",	"Helena"),
-            entry("Nebraska",	"Lincoln"),
-            entry("Nevada",	"Carson City"),
-            entry("New", "Hampshire Concord"),
-            entry("New Jersey", "Trenton"),
-            entry("New Mexico", "Santa Fe"),
-            entry("New York", "Albany"),
-            entry("North Carolina", "Raleigh")	,
-            entry("North Dakota", "Bismarck")	,
-            entry("Ohio",	"Columbus"),
-            entry("Oklahoma",	"Oklahoma City") ,
-            entry("Oregon",	"Salem"),
-            entry("Pennsylvania",	"Harrisburg"),
-            entry("Rhode Island", "Providence")	,
-            entry("South Carolina", "Columbia")	,
-            entry("South Dakota", "Pierre")	,
-            entry("Tennessee",	"Nashville"),
-            entry("Texas",	"Austin"),
-            entry("Utah",	"Salt Lake City") ,
-            entry("Vermont",	"Montpelier"),
-            entry("Virginia",	"Richmond"),
-            entry("Washington",	"Olympia"),
-            entry("West Virginia", "Charleston")	,
-            entry("Wisconsin",	"Madison"),
-            entry("Wyoming",	"Cheyenne")
-    );
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, String> statecapitals = readMap("src/StateCapitals.txt");
 
         System.out.println("STATES:");
         System.out.println("=======");
@@ -80,6 +27,26 @@ public class Main {
             output.add(k + " - " + map.get(k));
         }
         return output;
+    }
+
+    private static void storeMap(String filename, Map<String, String> map) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(filename));
+        for (String key : map.keySet()) {
+            out.println(key + "::" + map.get(key));
+        }
+        out.flush();
+        out.close();
+    }
+
+    private static Map<String, String> readMap(String filename) throws FileNotFoundException {
+        Map<String, String> out = new HashMap<>();
+        Scanner sc = new Scanner(
+                new BufferedReader(new FileReader(filename)));
+        while (sc.hasNextLine()) {
+            String[] line = sc.nextLine().split("::");
+            out.put(line[0], line[1]);
+        }
+        return out;
     }
 
     private static void printList(Collection list) {
