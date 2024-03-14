ddb2937754bdf73415c82fe4444c19c0b27e5a3f
Dan <Mackachooo@gmail.com>
2024-03-14 17:38:23 +0000

    Create a Service Layer Interface

---
+++ src/com/sg/classroster/service/ClassRosterServiceLayer.java
@@ -0,0 +1,19 @@
+package com.sg.classroster.service;
+
+public interface ClassRosterServiceLayer {
+
+    void createStudent(Student student) throws
+            ClassRosterDuplicateIdException,
+            ClassRosterDataValidationException,
+            ClassRosterPersistenceException;
+
+    List<Student> getAllStudents() throws
+            ClassRosterPersistenceException;
+
+    Student getStudent(String studentId) throws
+            ClassRosterPersistenceException;
+
+    Student removeStudent(String studentId) throws
+            ClassRosterPersistenceException;
+
+}
