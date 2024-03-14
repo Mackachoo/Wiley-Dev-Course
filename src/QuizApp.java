598728b7d14cedf717b2ef0cc39b8288b65f63f5
Dan <Mackachooo@gmail.com>
2024-03-14 12:27:35 +0000

    WIP

---
+++ src/QuizApp.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuizApp {
+    Map<String, ArrayList<Integer>> studentscores = new HashMap<>();
+
+
+
+}
