2e1ed395f517842b2025fa9cd44a8ff4e87aa194
Dan <Mackachooo@gmail.com>
2024-03-14 11:43:49 +0000

    Step 6: Get Student

--- src/com/sg/classroster/controller/ClassRosterController.java
+++ src/com/sg/classroster/controller/ClassRosterController.java
@@ -32,7 +32,7 @@ public class ClassRosterController {
                     createStudent();
                     break;
                 case 3:
-                    io.print("VIEW STUDENT");
+                    viewStudent();
                     break;
                 case 4:
                     io.print("REMOVE STUDENT");
@@ -60,6 +60,14 @@ public class ClassRosterController {
         List<Student> studentList = dao.getAllStudents();
         view.displayStudentList(studentList);
     }
+
+    private void viewStudent() {
+        view.displayDisplayStudentBanner();
+        String studentId = view.getStudentIdChoice();
+        Student student = dao.getStudent(studentId);
+        view.displayStudent(student);
+    }
+
 
 
 }
--- src/com/sg/classroster/ui/ClassRosterView.java
+++ src/com/sg/classroster/ui/ClassRosterView.java
@@ -18,6 +18,11 @@ public class ClassRosterView {
         return io.readInt("Please select from the above choices.", 1, 5);
     }
 
+    // Create Student
+    public void displayCreateStudentBanner() {
+        io.print("=== Create Student ===");
+    }
+
     public Student getNewStudentInfo() {
         String studentId = io.readString("Please enter Student ID");
         String firstName = io.readString("Please enter First Name");
@@ -30,12 +35,13 @@ public class ClassRosterView {
         return currentStudent;
     }
 
-    public void displayCreateStudentBanner() {
-        io.print("=== Create Student ===");
-    }
-
     public void displayCreateSuccessBanner() {
         io.readString("Student successfully created.  Please hit enter to continue");
+    }
+
+    // Display All Students
+    public void displayDisplayAllBanner() {
+        io.print("=== Display All Students ===");
     }
 
     public void displayStudentList(List<Student> studentList) {
@@ -49,8 +55,25 @@ public class ClassRosterView {
         io.readString("Please hit enter to continue.");
     }
 
-    public void displayDisplayAllBanner() {
-        io.print("=== Display All Students ===");
+    // Display Single Student
+    public void displayDisplayStudentBanner () {
+        io.print("=== Display Student ===");
+    }
+
+    public String getStudentIdChoice() {
+        return io.readString("Please enter the Student ID.");
+    }
+
+    public void displayStudent(Student student) {
+        if (student != null) {
+            io.print(student.getStudentId());
+            io.print(student.getFirstName() + " " + student.getLastName());
+            io.print(student.getCohort());
+            io.print("");
+        } else {
+            io.print("No such student.");
+        }
+        io.readString("Please hit enter to continue.");
     }
 
 
