f1f1b58563dc69702db8148f9945996d66a439e7
Dan <Mackachooo@gmail.com>
2024-03-15 11:28:44 +0000

    Create the Service Layer Implementation

--- src/com/sg/classroster/service/ClassRosterDuplicateIdException.java
+++ src/com/sg/classroster/service/ClassRosterDuplicateIdException.java
@@ -1,6 +1,7 @@
 package com.sg.classroster.service;
 
 public class ClassRosterDuplicateIdException extends Exception {
+
     public ClassRosterDuplicateIdException(String message) {
         super(message);
     }
--- src/com/sg/classroster/service/ClassRosterServiceLayerImpl.java
+++ src/com/sg/classroster/service/ClassRosterServiceLayerImpl.java
@@ -1,5 +1,6 @@
 package com.sg.classroster.service;
 
+import com.sg.classroster.dao.ClassRosterDao;
 import com.sg.classroster.dao.ClassRosterPersistenceException;
 import com.sg.classroster.dto.Student;
 
@@ -8,30 +9,71 @@ import java.util.List;
 public class ClassRosterServiceLayerImpl implements
         ClassRosterServiceLayer {
 
+    ClassRosterDao dao;
+
+    public ClassRosterServiceLayerImpl(ClassRosterDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public void createStudent(Student student) throws
             ClassRosterDuplicateIdException,
             ClassRosterDataValidationException,
             ClassRosterPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        // First check to see if there is alreay a student
+        // associated with the given student's id
+        // If so, we're all done here -
+        // throw a ClassRosterDuplicateIdException
+        if (dao.getStudent(student.getStudentId()) != null) {
+            throw new ClassRosterDuplicateIdException(
+                    "ERROR: Could not create student.  Student Id "
+                            + student.getStudentId()
+                            + " already exists");
+        }
+
+        // Now validate all the fields on the given Student object.
+        // This method will throw an
+        // exception if any of the validation rules are violated.
+        validateStudentData(student);
+
+        // We passed all our business rules checks so go ahead
+        // and persist the Student object
+        dao.addStudent(student.getStudentId(), student);
+
     }
 
     @Override
     public List<Student> getAllStudents() throws
             ClassRosterPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return dao.getAllStudents();
     }
 
     @Override
     public Student getStudent(String studentId) throws
             ClassRosterPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return dao.getStudent(studentId);
     }
 
     @Override
     public Student removeStudent(String studentId) throws
             ClassRosterPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return dao.removeStudent(studentId);
+    }
+
+    private void validateStudentData(Student student) throws
+            ClassRosterDataValidationException {
+
+        if (student.getFirstName() == null
+                || student.getFirstName().trim().isEmpty()
+                || student.getLastName() == null
+                || student.getLastName().trim().isEmpty()
+                || student.getCohort() == null
+                || student.getCohort().trim().isEmpty()) {
+
+            throw new ClassRosterDataValidationException(
+                    "ERROR: All fields [First Name, Last Name, Cohort] are required.");
+        }
     }
 
 }
