597d24e20ca78fbedbf8906cc8ce22eded14c9a9
Dan <Mackachooo@gmail.com>
2024-03-20 15:05:57 +0000

    Implemented all of DAO
    Removed vending machine step

--- src/com/sg/vendingmachine/dao/Dao.java
+++ src/com/sg/vendingmachine/dao/Dao.java
@@ -1,10 +1,8 @@
 package com.sg.vendingmachine.dao;
 
 import com.sg.vendingmachine.dto.Item;
-import com.sg.vendingmachine.dto.VendingMachine;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Map;
 
 public interface Dao {
@@ -12,11 +10,11 @@ public interface Dao {
     // Returns all items
     public Map<Item, Integer> getAll();
 
-    // Gets price of item
-    public BigDecimal getPrice(Item item);
-
     // Checks if item is present and returns it, null if no item
     public Item itemAccess(Item item);
+
+    // Gets price of item
+    public BigDecimal getPrice(Item item);
 
     // Remove item
     public void removeItem(Item item);
--- src/com/sg/vendingmachine/dao/DaoFileImpl.java
+++ src/com/sg/vendingmachine/dao/DaoFileImpl.java
@@ -1,34 +1,82 @@
 package com.sg.vendingmachine.dao;
 
 import com.sg.vendingmachine.dto.Item;
-import com.sg.vendingmachine.dto.VendingMachine;
 
-import javax.swing.*;
 import java.io.*;
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DaoFileImpl implements Dao {
 
-    VendingMachine vm = new VendingMachine();
+    private Map<Item, Integer> items;
+    private final String filename = "vendingmachine.txt";
 
-    @Override
-    public Map<Item, Integer> getAll() {
-        return vm.getItems();
+    private void readFile() {
+        Scanner scanner;
+
+        // Create Scanner for reading the file
+        try {
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(filename)));
+
+            String currentLine;
+            items = new HashMap<>();
+            while (scanner.hasNextLine()) {
+                String[] inputs = scanner.nextLine().split("::");
+                items.put(new Item(inputs[0], new BigDecimal(inputs[1])), Integer.parseInt(inputs[2]));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("-_- Could not load roster data into memory.");
+        }
+
+    }
+
+    private void writeFile() {
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(filename));
+
+            for (Item item : items.keySet()) {
+                out.println(item + "::" + items.get(item));
+                out.flush();
+            }
+            out.close();
+        } catch (IOException e) {
+            System.out.println("-_- Could not load roster data into memory.");
+        }
     }
 
     @Override
-    public BigDecimal getPrice(Item item) {
-        return null;
+    public Map<Item, Integer> getAll() {
+        readFile();
+        return items;
     }
 
     @Override
     public Item itemAccess(Item item) {
-        return null;
+        readFile();
+        List<Item> keys = items.keySet().stream().collect(Collectors.toList());
+        int i = keys.indexOf(item);
+        if (i == -1) return null;
+        return keys.get(i);
+    }
+
+    @Override
+    public BigDecimal getPrice(Item item) {
+        readFile();
+        return itemAccess(item).getCost();
     }
 
     @Override
     public void removeItem(Item item) {
-
+        readFile();
+        items.replace(item, items.get(item) - 1);
+        writeFile();
     }
+
+
 }
