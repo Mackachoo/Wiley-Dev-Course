6be1d7d1f55e701d2e716cc1835e0015aea31be1
Dan <Mackachooo@gmail.com>
2024-03-20 15:34:00 +0000

    Update ServiceLayerImpl.java

--- src/com/sg/vendingmachine/service/ServiceLayerImpl.java
+++ src/com/sg/vendingmachine/service/ServiceLayerImpl.java
@@ -21,41 +21,6 @@ public class ServiceLayerImpl implements
     }
 
     @Override
-    public void createStudent(Item item) throws
-            DuplicateIdException,
-            DataValidationException,
-            PersistenceException {
-
-        // First check to see if there is already a student
-        // associated with the given student's id
-        // If so, we're all done here -
-        // throw a ClassRosterDuplicateIdException
-        if (dao.getStudent(item.getStudentId()) != null) {
-            throw new DuplicateIdException(
-                    "ERROR: Could not create student.  Student Id "
-                            + item.getStudentId()
-                            + " already exists");
-        }
-
-        // Now validate all the fields on the given Student object.
-        // This method will throw an
-        // exception if any of the validation rules are violated.
-        validateStudentData(item);
-
-        // We passed all our business rules checks so go ahead
-        // and persist the Student object
-        dao.addStudent(item.getStudentId(), item);
-
-        // The student was successfully created, now write to the audit log
-        auditDao.writeAuditEntry(
-                "Student " + item.getStudentId() + " CREATED.");
-
-    }
-    public ServiceLayerImpl(Dao dao) {
-    }
-
-
-    @Override
     public Map<Item, Integer> getAll() {
         return dao.getAll();
     }
