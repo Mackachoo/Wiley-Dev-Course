452c0c6ffebef61a37e2eb68300a95d4bd139cb2
Ayusuf02221 <101650187+Ayusuf02221@users.noreply.github.com>
2024-03-20 15:28:52 +0000

    service

--- src/com/sg/vendingmachine/service/ServiceLayer.java
+++ src/com/sg/vendingmachine/service/ServiceLayer.java
@@ -5,11 +5,12 @@ import com.sg.vendingmachine.dto.Item;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 public interface ServiceLayer {
 
     // This returns a list of all items in file.
-    public List<Item> getAll();
+    public Map<Item, Integer> getAll();
 
     // Removes an item if present and returns VendResult.
     public VendResult vendItem(Item item, BigDecimal money);
--- src/com/sg/vendingmachine/service/ServiceLayerImpl.java
+++ src/com/sg/vendingmachine/service/ServiceLayerImpl.java
@@ -6,6 +6,9 @@ import com.sg.vendingmachine.dto.Item;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+
+import static com.sg.vendingmachine.service.VendResult.*;
 
 public class ServiceLayerImpl implements
         ServiceLayer {
@@ -13,77 +16,32 @@ public class ServiceLayerImpl implements
     private Dao dao;
 
 
-    public ServiceLayerImpl(Dao dao, AuditDao auditDao) {
-        this.dao = dao;
-        this.auditDao = auditDao;
+    public ServiceLayerImpl(Dao dao) {
+    }
+
+
+    @Override
+    public Map<Item, Integer> getAll() {
+        return dao.getAll();
     }
 
     @Override
-    public void createStudent(Item item) throws
-            DuplicateIdException,
-            DataValidationException,
-            PersistenceException {
-
-        // First check to see if there is already a student
-        // associated with the given student's id
-        // If so, we're all done here -
-        // throw a ClassRosterDuplicateIdException
-        if (dao.getStudent(item.getStudentId()) != null) {
-            throw new DuplicateIdException(
-                    "ERROR: Could not create student.  Student Id "
-                            + item.getStudentId()
-                            + " already exists");
+    public VendResult vendItem(Item item, BigDecimal money) {
+        if(dao.itemAccess(item) == null){
+            return INVALIDITEM;
         }
-
-        // Now validate all the fields on the given Student object.
-        // This method will throw an
-        // exception if any of the validation rules are violated.
-        validateStudentData(item);
-
-        // We passed all our business rules checks so go ahead
-        // and persist the Student object
-        dao.addStudent(item.getStudentId(), item);
-
-        // The student was successfully created, now write to the audit log
-        auditDao.writeAuditEntry(
-                "Student " + item.getStudentId() + " CREATED.");
-
-    }
-
-    @Override
-    public List<Item> getAllStudents() throws
-            PersistenceException {
-        return dao.getAllStudents();
-    }
-
-    @Override
-    public Item getStudent(String studentId) throws
-            PersistenceException {
-        return dao.getStudent(studentId);
-    }
-
-    @Override
-    public Item removeStudent(String studentId) throws PersistenceException {
-        Item removedItem = dao.removeStudent(studentId);
-        // Write to audit log
-        auditDao.writeAuditEntry("Student " + studentId + " REMOVED.");
-        return removedItem;
-    }
-
-    private void validateStudentData(Item item) throws
-            DataValidationException {
-
-        if (item.getFirstName() == null
-                || item.getFirstName().trim().isEmpty()
-                || item.getLastName() == null
-                || item.getLastName().trim().isEmpty()
-                || item.getCohort() == null
-                || item.getCohort().trim().isEmpty()) {
-
-            throw new DataValidationException(
-                    "ERROR: All fields [First Name, Last Name, Cohort] are required.");
+        else if(dao.getCount(item) == 0){
+            return NOMOREITEMS;
+        }else if(money.compareTo(dao.getPrice(item)) > 0){
+            return INVALIDFUNDS;
         }
+        return BOUGHT;
     }
 
+    @Override
+    public BigDecimal calculateChange(Item item, BigDecimal money) {
+        money = money.subtract(dao.getPrice(item));
+        return money;
+    }
 }
 
